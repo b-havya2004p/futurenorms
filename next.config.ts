1dbba92ec325c26cf424af7de2981bf72260163d
b-havya2004p <bhavya2004p@gmail.com>
2026-07-15 13:29:16 +0530

    Update Next.js configuration options

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  compress: true,
+
+  poweredByHeader: false,
+
+  reactStrictMode: true,
+
+  images: {
+    formats: ["image/avif", "image/webp"],
+    minimumCacheTTL: 31536000,
+  },
 };
 
 export default nextConfig;
